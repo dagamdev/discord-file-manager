b09771348e3214e29ac5e996e59b9958f6041612
Dagadev <gacub20ru@gmail.com>
2024-02-06 18:41:28 -0600

    Update second token env name

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,4 +1,4 @@
-export const TOKEN = import.meta.env.VITE_TOKEN
+export const SECOND_TOKEN = import.meta.env.VITE_SECOND_TOKEN
 export const PRINCIPAL_TOKEN = import.meta.env.VITE_PRINCIPAL_TOKEN
 export const API_URL = import.meta.env.VITE_API_URL
 export const ENDPOINT = API_URL+'/api/v1/'

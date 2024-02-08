cceee8f2099eba27f4dc37408bfdd2c77b13fcb8
Dagadev <gacub20ru@gmail.com>
2024-02-08 13:05:55 -0600

    Optional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import './App.css'
 import './utils/socket'
+import { useState } from 'react'
 import Tooltip from './components/shared/tooltip/Tooltip'
 import MoveFiles from './components/moveFiles/MoveFiles'
 import NotificationsList from './components/shared/notifications/NotificationsList'
 import ImageViewer from './components/shared/image/ImageViewer'
 import GuildData from './components/guild/GuildData'
+import MoveFilesProvider from './providers/MoveFilesProvider'
 
 function App() {
+  const [mode] = useState<'files' | 'guilds'>('files')
 
   return (
     <>
-      {true ? 
-        <MoveFiles /> :
+      {mode === 'files' ? 
+        <MoveFilesProvider>
+          <MoveFiles />
+        </MoveFilesProvider> :
         <GuildData />
       }
       <NotificationsList />
